2c93a8a63ba276ae39f2c9dbb8ea1d3b7ae3f2c7
mgelios <master_gelios@tut.by>
2017-09-27 15:11:10 +0300

    added dynamic templates

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})

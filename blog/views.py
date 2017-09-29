cc6579664587e538328991aad997c1aaa4bc6894
mgelios <master_gelios@tut.by>
2017-09-29 16:47:58 +0300

    added edit post function

--- blog/views.py
+++ blog/views.py
@@ -38,3 +38,16 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if (form.is_valid()):
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})

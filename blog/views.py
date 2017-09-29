3f4a6fd868e8bf29cd44cfcf91e1ab2169704b9f
mgelios <master_gelios@tut.by>
2017-09-29 13:58:56 +0300

    added drafts page func

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from .models import Post
 from .forms import PostForm
 
 def post_list(request):
-    posts = Post.objects.order_by('-published_date')
+    posts = Post.objects.filter(published_date__isnull=False).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -19,9 +19,12 @@ def post_new(request):
         if (form.is_valid):
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})

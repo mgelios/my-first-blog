ff379d0f3b37a9362b98abf4196167707e1351ca
mgelios <master_gelios@tut.by>
2017-10-19 16:55:55 +0300

    added widget view, updated styling

--- dashboard/dashes/weather/OpenWeather.py
+++ dashboard/dashes/weather/OpenWeather.py
@@ -1,7 +1,9 @@
+import json
 import urllib
 
 def forecast():
     base_url = 'http://api.openweathermap.org/data/2.5/weather?'
-    query = 'q=Minsk,by&appid=0502d0f1f744a28dd8065598094fa1db'
-    content = urllib.request.urlopen(base_url + query).read()
-    return content
+    query = 'q=Minsk,by&appid=0502d0f1f744a28dd8065598094fa1db&units=metric'
+    content = urllib.request.urlopen(base_url + query).read().decode('utf-8')
+    json_content = json.loads(content)
+    return json_content

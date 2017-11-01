d6fddf769d5dd6eeaea946a56c200f0033004ed9
mgelios <master_gelios@tut.by>
2017-11-01 18:03:08 +0300

    disabled bot for server

--- mysite/settings/local.py
+++ mysite/settings/local.py
@@ -4,24 +4,24 @@ DEBUG = True
 
 SECRET_KEY = 'local secret key'
 
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'blog',
-    'landing',
-    'dashboard',
-    'django_telegrambot',
-)
+# INSTALLED_APPS = (
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+#     'rest_framework',
+#     'blog',
+#     'landing',
+#     'dashboard',
+#     'django_telegrambot',
+# )
 
-DJANGO_TELEGRAMBOT = {
-    'MODE': 'POLLING',
-    'WEBHOOK_SITE': 'localhost',
-    'BOTS': [{
-        'TOKEN': '460933242:AAEh67xQVBeT37EwN84iudv80tbYBsOY1QA'
-    }]
-}
+# DJANGO_TELEGRAMBOT = {
+#     'MODE': 'POLLING',
+#     'WEBHOOK_SITE': 'localhost',
+#     'BOTS': [{
+#         'TOKEN': '460933242:AAEh67xQVBeT37EwN84iudv80tbYBsOY1QA'
+#     }]
+# }

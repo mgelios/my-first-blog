d84bc71612c50013102ac1d595f6672ccdb5777f
mgelios <master_gelios@tut.by>
2017-11-20 15:27:43 +0300

    telegram bot

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = (
 
 DJANGO_TELEGRAMBOT = {
     'MODE': 'WEBHOOK',
-    'WEBHOOK_SITE': 'mgelios.pythonanywhere.com',
+    'WEBHOOK_SITE': 'https://mgelios.pythonanywhere.com',
     'BOTS': [{
         'TOKEN': '460933242:AAEh67xQVBeT37EwN84iudv80tbYBsOY1QA'
     }]

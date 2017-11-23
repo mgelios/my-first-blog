5f60d19ada6b463c83606e4ee6b27045785f6c80
mgelios <master_gelios@tut.by>
2017-11-23 17:33:56 +0300

    updated crypto sheet, added new traditional currencies

--- dashboard/dashes/currency/NBRBCurrency.py
+++ dashboard/dashes/currency/NBRBCurrency.py
@@ -4,26 +4,35 @@ import urllib
 def get_currencies():
     json_content = []
 
-    currency = get_currency('298')
+    currency = get_currency('RUB')
     if currency and currency != '':
         json_content.append(json.loads(currency))
 
-    currency = get_currency('292')
+    currency = get_currency('USD')
     if currency and currency != '':
         json_content.append(json.loads(currency))
 
-    currency = get_currency('145')
+    currency = get_currency('EUR')
     if currency and currency != '':
         json_content.append(json.loads(currency))
         
+    currency = get_currency('UAH')
+    if currency and currency != '':
+        json_content.append(json.loads(currency))
+
+    currency = get_currency('CNY')
+    if currency and currency != '':
+        json_content.append(json.loads(currency))
+
     return json_content
 
 def get_currency(id):
     base_url = 'http://www.nbrb.by/API/'
-    query = 'ExRates/Rates/' + id
+    query = 'ExRates/Rates/' + id + '?ParamMode=2'
     content = ''
     try:
         content = urllib.request.urlopen(base_url + query).read().decode('utf-8')
-    except urllib.error.HTTPError:
+    except urllib.error.HTTPError as e:
         print("error during fetching currency")
+        print(e)
     return content

bf40ea611f79acd66aa385ae44f484191baafd45
mgelios <master_gelios@tut.by>
2017-12-08 16:24:39 +0300

    fix for bot

--- dashboard/telegrambot.py
+++ dashboard/telegrambot.py
@@ -1,4 +1,4 @@
-from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
+from telegram.ext import CommandHandler, MessageHandler, Filters, JobQueue
 from django_telegrambot.apps import DjangoTelegramBot
 from dashboard.dashes.weather import OpenWeather
 from dashboard.dashes.currency import NBRBCurrency
@@ -66,7 +66,8 @@ def main():
 
     dp.add_handler(MessageHandler([Filters.text], echo))
 
-    dp.job_queue.run_repeating(weather_job_callback, interval=60, first=0)
+    dp.job_queue = JobQueue(bot=dp.bot)
+    #run_repeating(weather_job_callback, interval=60, first=0)
 
 
     dp.add_error_handler(error)

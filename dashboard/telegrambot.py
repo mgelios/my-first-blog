d5b6286f09a668b3ad8e4b4ee1ee08416be8dfeb
mgelios <master_gelios@tut.by>
2017-12-08 16:26:00 +0300

    telebot fix

--- dashboard/telegrambot.py
+++ dashboard/telegrambot.py
@@ -67,7 +67,7 @@ def main():
     dp.add_handler(MessageHandler([Filters.text], echo))
 
     dp.job_queue = JobQueue(bot=dp.bot)
-    #run_repeating(weather_job_callback, interval=60, first=0)
+    dp.job_queue.run_repeating(weather_job_callback, interval=60, first=0)
 
 
     dp.add_error_handler(error)

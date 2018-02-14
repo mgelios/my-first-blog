22f5b2e8a9003c2105dd3df24b575bdc3e5be0d0
mgelios <master_gelios@tut.by>
2018-02-14 13:04:28 +0300

    another debug commit for testing webhook

--- bots/urls.py
+++ bots/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, re_path, include
 from . import views
 
 urlpatterns = [
-    url(r'^bots/telegram/$', views.telegram_bot, name='telegram_bot'),
+    url(r'^bots/telegram/460933242:AAEh67xQVBeT37EwN84iudv80tbYBsOY1QA/$', views.telegram_bot, name='telegram_bot'),
 ]

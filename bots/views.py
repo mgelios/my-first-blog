9ad6560bd242023b6ace7a17e92812bc1028ce8d
mgelios <master_gelios@tut.by>
2018-02-14 13:29:31 +0300

    another debug

--- bots/views.py
+++ bots/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 from django.views.decorators.csrf import csrf_exempt
 
 import telegram
+import logging
 
 from .apps import TelegramBot
 
 my_telegram_id = '@master_gelios'
 
+logger = logging.getLogger(__name__)
 
 @csrf_exempt
 def telegram_bot(request, bot_token):
@@ -17,8 +19,9 @@ def telegram_bot(request, bot_token):
         updater = TelegramBot.updater
         dispatcher = TelegramBot.dispatcher
         try:
+            logger.debug('### bot, updater, dispatcher recieved')
             update = telegram.Update.de_json(data, bot)
             dispatcher.process_update(update)
         except TelegramError as er:
-            print("Error occured during while handling message")
-    return HttpResponse('<h1>It okayyy</h1>')
+            logger.warn('### ')
+    return JsonResponse({})

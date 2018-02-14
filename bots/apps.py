37c51739216d3bbdd95aa16298dc16f379cc7c41
mgelios <master_gelios@tut.by>
2018-02-14 13:40:13 +0300

    debug

--- bots/apps.py
+++ bots/apps.py
@@ -2,7 +2,6 @@ from django.apps import AppConfig
 from django.conf import settings
 
 import telegram
-import logging
 
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
@@ -12,8 +11,6 @@ from telegram.error import InvalidToken, TelegramError, BadRequest
 TELEGRAM_BOT_TOKEN = '460933242:AAEh67xQVBeT37EwN84iudv80tbYBsOY1QA'
 TELEGRAM_BOT_SUFFIX = 'bots/telegram/'
 TELEGRAM_BOT_WEBHOOK_ADDR = 'https://mgelios.pythonanywhere.com/'
-
-logger = logging.getLogger(__name__)
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='Im a bot,  bloa blaoa bloa')
@@ -36,7 +33,7 @@ class TelegramBot(AppConfig):
             try:
                 bot.setWebhook(url=TELEGRAM_BOT_WEBHOOK_ADDR+TELEGRAM_BOT_SUFFIX+TELEGRAM_BOT_TOKEN+'/')
                 webhook_started = True
-                logger.debug('### webhook successfuly started')
+                print('### webhook successfuly started')
             except BadRequest as er:
-                logger.warn('### error occured during setting webhook')
+                print('### error occured during setting webhook')
 
--- bots/views.py
+++ bots/views.py
@@ -4,25 +4,22 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 import telegram
-import logging
 
 from .apps import TelegramBot
 
 my_telegram_id = '@master_gelios'
 
-logger = logging.getLogger(__name__)
-
 @csrf_exempt
 def telegram_bot(request, bot_token):
-    logger.debug('### telegram bot endoint called')
+    print('### telegram bot endoint called')
     if TelegramBot.webhook_started:
         bot = TelegramBot.bot
         updater = TelegramBot.updater
         dispatcher = TelegramBot.dispatcher
         try:
-            logger.debug('### bot, updater, dispatcher recieved')
+            print('### bot, updater, dispatcher recieved')
             update = telegram.Update.de_json(data, bot)
             dispatcher.process_update(update)
         except TelegramError as er:
-            logger.warn('### ')
+            print('### ')
     return JsonResponse({})

7b9abe82ac4f0fc2c06960736ed74799d5e77dbb
mgelios <master_gelios@tut.by>
2018-02-20 17:11:36 +0300

    updated styling of tasklist, and added deleting action

--- todo/urls.py
+++ todo/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns = [
     url(r'^todo/$', views.action_list, name='action_list'),
     url(r'^todo/task/new/$', views.create_action, name='create_action'),
+    url(r'^todo/task/(?P<pk>\d+)$',views.update_action, name='update_action'),
+    url(r'^todo/task/delete/(?P<pk>\d+)$',views.delete_action, name='delete_action'),
     url(r'^todo/category/new/', views.create_action_category, name='create_action_category'),
     url(r'^todo/category/(?P<pk>\d+)$',views.category_action_list, name='category_action_list'),
-    url(r'^todo/task/(?P<pk>\d+)$',views.update_action, name='update_action'),
+    url(r'^todo/category/update/(?P<pk>\d+)$',views.update_action_category, name='update_action_category'),
+    url(r'^todo/category/delete/(?P<pk>\d+)$',views.delete_action_category, name='delete_action_category'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -39,40 +39,72 @@ def category_action_list(request, pk):
 @login_required
 def update_action(request, pk):
     action = get_object_or_404(Action, pk=pk)
+    categories = ActionCategory.objects.filter(author=request.user)
     if request.method == 'POST':
         form = ActionForm(request.POST, instance=action)
         if (form.is_valid):
             action = form.save()
             action.save()
-            return redirect('action_list')
+            return redirect('category_action_list', pk=action.category.pk)
     else:
         form = ActionForm(instance=action)
         form.fields['category'].queryset = ActionCategory.objects.filter(author=request.user)
-    return render(request, 'todo/action_edit.html', {'form': form})
+    return render(request, 'todo/action_edit.html', {'form': form, 'is_update': True, 'categories': categories})
 
 @login_required
 def create_action(request):
+    categories = ActionCategory.objects.filter(author=request.user)
     if request.method == 'POST':
         form = ActionForm(request.POST)
         if (form.is_valid):
             action = form.save()
             action.save()
-            return redirect('action_list')
+            return redirect('category_action_list', pk=action.category.pk)
     else:
         form = ActionForm()
         form.fields['category'].queryset = ActionCategory.objects.filter(author=request.user)
-    return render(request, 'todo/action_edit.html', {'form': form})
+    return render(request, 'todo/action_edit.html', {'form': form, 'categories': categories})
 
 @login_required
 def create_action_category(request):
+    categories = ActionCategory.objects.filter(author=request.user)
     if request.method == 'POST':
         form = ActionCategoryForm(request.POST)
         if (form.is_valid):
             action_category = form.save()
             action_category.author = request.user
             action_category.save()
-            return redirect('action_list')
+            return redirect('category_action_list', pk=action_category.pk)
     else:
         form = ActionCategoryForm()
-    return render(request, 'todo/category_edit.html', {'form': form})
+    return render(request, 'todo/category_edit.html', {'form': form, 'categories': categories})
+
+@login_required
+def update_action_category(request, pk):
+    action_category = get_object_or_404(ActionCategory, pk=pk)
+    categories = ActionCategory.objects.filter(author=request.user)
+    if request.method == 'POST':
+        form = ActionCategoryForm(request.POST, instance=action_category)
+        if (form.is_valid):
+            action_category = form.save()
+            action_category.author = request.user
+            action_category.save()
+            return redirect('category_action_list', pk=action_category.pk)
+    else:
+        form = ActionCategoryForm(instance=action_category)
+    return render(request, 'todo/category_edit.html', {'form': form, 'categories': categories, 'is_update': True})
+
+
+@login_required
+def delete_action(request, pk):
+    action = get_object_or_404(Action, pk=pk)
+    category_pk = action.category.pk
+    action.delete()
+    return redirect('category_action_list', pk=category_pk)
+
+@login_required
+def delete_action_category(request, pk):
+    action_category = get_object_or_404(ActionCategory, pk=pk)
+    action_category.delete()
+    return redirect('action_list')
 

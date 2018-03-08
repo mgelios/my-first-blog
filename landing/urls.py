af1aef9a1f1ae3bd663a1ee181246d3bb59f0b77
mgelios <master_gelios@tut.by>
2018-03-08 10:54:33 +0300

    changed start page, updated greeting page style

--- landing/urls.py
+++ landing/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.apps_list, name='apps_list'),
+    url(r'^$', views.show_greeting, name='show_greeting'),
     url(r'^landing/messages/$', views.secret_message_list, name='secret_message_list'),
     url(r'^landing/about/$', views.info_about, name='info_about'),
     url(r'^landing/register/$', views.register, name='register'),
-    url(r'^landing/greeting/$', views.show_greeting, name='show_greeting'),
+    url(r'^landing/greeting/$', views.apps_list, name='apps_list'),
 ]
